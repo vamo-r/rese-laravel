ac31df92710aa37803a09247b963b8c59d1bdc35
vamo-r <vamor.toda@gmail.com>
2022-01-09 23:49:14 +0900

    [add] モデルの追加とリレーションの追加

--- app/Models/Shop.php
+++ app/Models/Shop.php
@@ -13,6 +13,14 @@ class Shop extends Model
         'id',
     ];
 
+    public function area() {
+        return $this->belongsTo(Area::class, 'area_id', 'id');
+    }
+
+    public function genre() {
+        return $this->belongsTo(Genre::class, 'genre_id', 'id');
+    }
+
     public function likes() {
         return $this->hasMany(Like::class);
     }
