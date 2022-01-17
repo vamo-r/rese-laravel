a20d94517053b9c4489ba6ae272394f167495053
vamo-r <vamor.toda@gmail.com>
2022-01-17 23:47:11 +0900

    [update] 予約機能実装

--- app/Http/Controllers/ReservationsController.php
+++ app/Http/Controllers/ReservationsController.php
@@ -2,62 +2,60 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Reservation;
 use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 class ReservationsController extends Controller
 {
     /**
      * Display a listing of the resource.
      *
-     * @return \Illuminate\Http\Response
+     * @return \Illuminate\Http\JsonResponse
      */
-    public function index()
+    public function index(Reservation $reservation)
     {
-        //
+        $reservations_data = $reservation->getReservations();
+
+        return response()->json(['reservations_data' => $reservations_data], Response::HTTP_OK);
     }
 
     /**
      * Store a newly created resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
+     * @return \Illuminate\Http\JsonResponse
      */
-    public function store(Request $request)
+    public function store(Request $request, Reservation $reservation)
     {
-        //
-    }
+        $request->validate([
+            'date' => ['required', 'string', 'max:10'],
+            'time' => ['required', 'string', 'max:5'],
+            'number' => ['required', 'integer'],
+        ]);
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        //
-    }
+        $reservation_data = $reservation->checkReservation($request);
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
+        if($reservation_data) {
+            return response()->json(['error' => 'この時間は既に予約があります'], Response::HTTP_CONFLICT);
+        } else if(!$reservation_data) {
+            $reservation->reserveShop($request);
+
+            return response()->json('Reservation completed', Response::HTTP_OK);
+        }
     }
 
     /**
      * Remove the specified resource from storage.
      *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\JsonResponse
      */
-    public function destroy($id)
+    public function destroy(Request $request, Reservation $reservation)
     {
-        //
+        $id = $request->id;
+        $reservation->deleteReservation($id);
+
+        return response()->json('Reservation delete', Response::HTTP_OK);
     }
 }
